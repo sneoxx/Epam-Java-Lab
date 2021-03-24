79a226f7a0bef3c4360863691d10a1adfe47e0ce
Rustam Zaraev <sneoxx@mail.ru>
2021-03-24 09:42:33 +0400

    Commit2 lesson1-try2 - Separate class for methods

--- src/main/java/zaraev/epam/com/Main.java
+++ src/main/java/zaraev/epam/com/Main.java
@@ -3,32 +3,29 @@ package zaraev.epam.com;
 import java.util.Arrays;
 
 public class Main {
-    static String str = "Hello World - это моя первая программа!";
-    static String str2 = new String("Я крутой Java программист"); // 3. Создать новую строку с помощью конструктора и занести ее в пул литералов
+
 
     public static void main(String[] args) {
+        String str = "Hello World - это моя первая программа!";
+        String str2 = new String("Я крутой Java программист");
+        String str3 = str2.concat(str2.concat((str2.concat(str2.concat(str2)))));
+        StringLesson1 myStr = new StringLesson1();
 
+        System.out.println("1. Получить длину строки : " + myStr.getLength(str));
+        System.out.println("2. Сравнить 2 строки без учета регистра: " + myStr.getEqualsIgnoreCase(str, str2));
+        System.out.println("4. Получить из строки массив символов:  " + Arrays.toString(myStr.getToCharArray(str)));
+        System.out.println("5. Получить из строки массив байтов:  " + Arrays.toString(myStr.getBytesString(str)));
+        System.out.println("6. Привести строку к верхнему регистру: " + myStr.getToUpperCase(str));
+        System.out.println("7. Найти первую позицию символа \"а\" в строке: " + myStr.getIndexOf(str,"a"));
+        System.out.println("8. Найти последнюю позицию символа \"а\" в строке: " + myStr.getLastIndexOf(str,"a"));
+        System.out.println("9. Проверить содержит-ли строка слово \"Sun\": " + myStr.getIndexOf(str,"Sun"));
+        System.out.println("10. Проверить оканчивается-ли строка на \"Oracle\": " + myStr.getEndsWith(str,"Oracle"));
+        System.out.println("11. Проверить начинается-ли строка на \"Java\": " + myStr.getStartsWith(str,"Java"));
+        System.out.println("12. Заменить все символы \"а\" в строке на символ \"о\": " + myStr.getReplace(str,'a', 'o'));
+        System.out.println("13. Получить подстроку с 44 символа по 90 символ: " + myStr.getSubstring(str3,39, 91));
+        System.out.println("14. Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива): " + Arrays.toString(myStr.getsSplit(str," ")));
+        System.out.println("15. Поменять последовательность символов в строке на обратную: " + myStr.getReverseString(str));
 
-        System.out.println("1. Получить длину строки : " + str.length());
-        System.out.println("2. Сравнить 2 строки без учета регистра: " + str.equalsIgnoreCase(str2));
-        System.out.println("4. Получить из строки массив символов:  " + Arrays.toString(str.toCharArray()));
-        System.out.println("5. Получить из строки массив байтов:  " + Arrays.toString(str.getBytes()));
-        System.out.println("6. Привести строку к верхнему регистру: " + str.toUpperCase());
-        System.out.println("7. Найти первую позицию символа \"а\" в строке: " + str.indexOf('a'));
-        System.out.println("8. Найти последнюю позицию символа \"а\" в строке: " + str.lastIndexOf('a'));
-        System.out.println("9. Проверить содержит-ли строка слово \"Sun\": " + str.indexOf("Sun"));
-        System.out.println("10. Проверить оканчивается-ли строка на \"Oracle\": " + str.endsWith("Oracle"));
-        System.out.println("11. Проверить начинается-ли строка на \"Java\": " + str.startsWith("Java"));
-        System.out.println("12. Заменить все символы \"а\" в строке на символ \"о\": " + str.replace('a', 'o'));
-        System.out.println("13. Получить подстроку с 44 символа по 90 символ: " + str2.concat(str2.concat((str2.concat(str2.concat(str2))))).substring(39, 91));
-        System.out.println("14. Разбить строку по символу пробел (т.е. чтобы каждое слово было отдельным элементом массива): " + Arrays.toString(str.split(" ")));
-        System.out.println("15. Поменять последовательность символов в строке на обратную: " + reverseString(str));
     }
 
-    public static String reverseString(String string) {
-        //напишите тут ваш код
-        StringBuilder builder = new StringBuilder(string);
-        builder.reverse();
-        return builder.toString();
-    }
 }
