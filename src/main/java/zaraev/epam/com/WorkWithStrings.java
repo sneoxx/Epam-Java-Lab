af60eb019d94a5586e3bccfef28fe984f3f96cbf
Rustam Zaraev <sneoxx@mail.ru>
2021-03-24 12:02:29 +0400

    Commit4 lesson1-try2 - fixed JavaDoc

--- src/main/java/zaraev/epam/com/WorkWithStrings.java
+++ src/main/java/zaraev/epam/com/WorkWithStrings.java
@@ -4,127 +4,127 @@ import java.util.Arrays;
 
 public class WorkWithStrings {
     /**
-     * Получить длину строки
-     * @param string - принимает строку на вход
-     * @return int возвращает число int
+     * Определение длины строки
+     * @param string - исходная строка
+     * @return int - длина строки
      */
     public int getLength(String string){
         return string.length();
     }
 
     /**
-     * Сравнить 2 строки без учета регистра
-     * @param string принимает строку на вход
-     * @param string2 принимает строку на вход
-     * @return boolean возвращает true или false
+     * Сравнение 2 строк без учета регистра
+     * @param string - первая строка
+     * @param string2  - вторая строка
+     * @return boolean - возвращает true или false, равны или нет
      */
     public boolean getEqualsIgnoreCase(String string, String string2){
         return string.equalsIgnoreCase(string2);
     }
 
     /**
-     * Получить из строки массив символов
-     * @param string принимает строку на вход
-     * @return char[] мозвращает массив символов
+     * Получение из строки массива символов
+     * @param string - исходная строка
+     * @return char[] - возвращает массив символов
      */
     public char[] getToCharArray(String string){
         return string.toCharArray();
     }
 
     /**
-     * Получить из строки массив байтов
-     * @param string принимает строку на вход
-     * @return byte[] возвращает массив байтов
+     * Получение из строки массива байтов
+     * @param string - исходная строка
+     * @return byte[] - возвращает массив байтов
      */
     public byte[] getBytesString(String string){
         return string.getBytes();
     }
 
     /**
-     * Привести строку к верхнему регистру
-     * @param string принимает строку на вход
-     * @return String возвращает строку
+     * Приведение строки к верхнему регистру
+     * @param string - исходная строка
+     * @return String - возвращает строку в верхнем регистре
      */
     public String getToUpperCase(String string){
         return string.toUpperCase();
     }
 
     /**
-     * Найти первую позицию символа
-     * @param string принимает строку в которой будем искать
-     * @param string2 принимает строку, которую будем искать
-     * @return int возвращает позицию символа
+     * Нахождение первой позицию символа в строке
+     * @param string - строка в которой будем искать
+     * @param string2 - строка, которую будем искать
+     * @return int - первая позиция символа в строке, если его нет, вернет -1
      */
     public int getIndexOf(String string, String string2){
         return string.indexOf(string2);
     }
 
     /**
-     * Найти последнюю позицию символа
-     * @param string принимает строку в которой будем искать
-     * @param string2 принимает строку, которую будем искать
-     * @return int возвращает позицию символа
+     * Нахождение последней позиции символа в строке
+     * @param string - строка в которой будем искать
+     * @param string2 - строка, которую будем искать
+     * @return int  - песледняя позиция символа в строке, если его нет, вернет -1
      */
     public int getLastIndexOf(String string, String string2){
         return string.lastIndexOf(string2);
     }
 
     /**
-     * Проверить оканчивается-ли строка на слово
-     * @param string принимает строку в которой будем искать
-     * @param string2 принимает строку, которую будем искать
-     * @return boolean возвращает true или false
+     * Проверка оканчивается-ли строка на слово
+     * @param string - строка в которой будем искать
+     * @param string2 - строка, которую будем искать
+     * @return boolean - возвращает true или false, оканчивается строка на слово или нет
      */
     public boolean getEndsWith(String string, String string2){
         return string.endsWith(string2);
     }
 
     /**
-     * Проверить начинается-ли строка на слово
-     * @param string принимает строку в которой будем искать
-     * @param string2 принимает строку, которую будем искать
-     * @return boolean возвращает true или false
+     * Проверка начинается-ли строка на слово
+     * @param string - строка в которой будем искать
+     * @param string2 - строка, которую будем искать
+     * @return boolean - возвращает true или false, начинается строка на слово или нет
      */
     public boolean getStartsWith(String string, String string2){
         return string.startsWith(string2);
     }
 
     /**
-     * Заменить все символы в строке
-     * @param string принимает строку на вход
-     * @param char1 принимает символ который будем менять
-     * @param char2 принимает символ на который будем менять
-     * @return String возвращает строку
+     * Замена всех символов в строке на указанный символ
+     * @param string - исходная строка
+     * @param char1 - символ который будем менять
+     * @param char2 - символ на который будем менять
+     * @return String - строка с замененными символами
      */
     public String getReplace(String string, char char1, char char2){
         return string.replace(char1, char2);
     }
 
     /**
-     * Получить подстроку с index1 символа по index2 символ:
-     * @param string принимает строку на вход
-     * @param index1 принимает индекс первого символа
-     * @param index2 принимает индекс второго символа
-     * @return String возвращает строку
+     * Получение из строки, подстроки с index1 символа по index2 символ
+     * @param string - исходная строка
+     * @param index1 - индекс первого символа подстроки
+     * @param index2 - индекс последнего символа подстроки
+     * @return String  - итоговая подстрока с index1 символа по index2 символ
      */
     public String getSubstring(String string, int index1, int index2){
         return string.substring(index1, index2);
     }
 
     /**
-     * разбить строку по символу
-     * @param string принимает строку на вход
-     * @param regex по какому символу разбить
-     * @return String[] возвращает массив строк
+     * Разбитие строки на массив строк по символу
+     * @param string - исходная строка
+     * @param regex - символ по которому разбить строку
+     * @return String[] - массив строк разбитый по символу
      */
     public String[] getsSplit(String string, String regex){
         return string.split(regex);
     }
 
     /**
-     * Поменять последовательность символов в строке на обратную
-     * @param string принимает строку на вход
-     * @return String возвращает строку
+     * Поменять последовательность символов строки на обратную
+     * @param string - исходная строка
+     * @return String - строка с обратной последовательностью символов
      */
     public String getReverseString(String string) {
         StringBuilder builder = new StringBuilder(string);
