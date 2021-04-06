f6540d36203c60b047f65381f250b37fa99898ac
Rustam Zaraev <sneoxx@mail.ru>
2021-04-06 17:25:27 +0400

    Commit2 lesson5-try1 start

--- src/main/java/zaraev/epam/com/Main.java
+++ src/main/java/zaraev/epam/com/Main.java
@@ -2,9 +2,21 @@ package zaraev.epam.com;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.*;
+
 @Slf4j
 public class Main {
     public static void main(String[] args) {
         log.debug("Старт программы");
+        try (FileOutputStream fos = new FileOutputStream("test.txt");
+             BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+            String s1 = "Наша первая строка\n";
+            String s2 = "Наша вторая строка";
+            bos.write((s1.getBytes()));
+            bos.write((s2.getBytes()));
+            bos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
