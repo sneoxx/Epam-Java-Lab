de71babdd327239af0acddd639ec0d8f5c90ed33
Rustam Zaraev <sneoxx@mail.ru>
2021-04-06 08:25:01 +0400

    Commit2 master add Logger

--- src/main/java/zaraev/epam/com/Main.java
+++ src/main/java/zaraev/epam/com/Main.java
@@ -1,7 +1,10 @@
 package zaraev.epam.com;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-
+        log.debug("Старт программы");
     }
 }
