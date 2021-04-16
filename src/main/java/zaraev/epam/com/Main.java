774c90a843a9cf5be7041bfb2a86578b075db444
Rustam Zaraev <sneoxx@mail.ru>
2021-04-16 16:14:34 +0400

    Commit5 lesson7-try1 fix1

--- src/main/java/zaraev/epam/com/Main.java
+++ src/main/java/zaraev/epam/com/Main.java
@@ -2,32 +2,51 @@ package zaraev.epam.com;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.InvocationTargetException;
+
 @Slf4j
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            log.debug("Старт программы");
-            MyClassLoader classLoader = new MyClassLoader();
-            Class clazz = classLoader.loadClass
-                    ("C:/myClasses/TestClass");
-            Object obj = clazz.newInstance();
-            System.out.println(obj);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
+        loadClassThroughMyClassLoaderAndPrintln();
         receiveOutOfMemoryError();
         receiveStackOverflowError();
     }
 
+    /**
+     * Создание объекта указанного класса через загрузчик классов MyClassLoader
+     */
+    public static void loadClassThroughMyClassLoaderAndPrintln() {
+        try {
+            log.debug("Старт программы");
+            MyClassLoader classLoader = new MyClassLoader();
+            Class clazz = classLoader.loadClass("TestClass");
+            Object obj = clazz.getDeclaredConstructor().newInstance();
+            System.out.println(obj);
+            log.debug("Объект указанного класса успешно выведен в консоль");
+        } catch (ClassNotFoundException e) {
+            log.error("Класс не найден", e);
+        } catch (InstantiationException e) {
+            log.error("Экземпляр класса не может быть создан", e);
+        } catch (IllegalAccessException e) {
+            log.error("Доступ к классу запрещен", e);
+        } catch (NoSuchMethodException e) {
+            log.error("Метод не найден", e);
+        } catch (InvocationTargetException e) {
+            log.error("Исключение вызванного метода или конструктора", e);
+        }
+    }
+
+    /**
+     * Генерация ошибки переполнения Heap - OutOfMemoryError
+     */
     public static void receiveOutOfMemoryError() {
         Long[] longs = new Long[6000 * 6000];
     }
 
+    /**
+     * Генерация ошибки переполнения стэка - StackOverflowError
+     */
     public static void receiveStackOverflowError() {
         receiveStackOverflowError();
     }
--- src/main/java/zaraev/epam/com/MyClassLoader.java
+++ src/main/java/zaraev/epam/com/MyClassLoader.java
@@ -1,22 +1,41 @@
 package zaraev.epam.com;
 
-import java.io.*;
 
-public class MyClassLoader extends ClassLoader{
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+@Slf4j
+
+public class MyClassLoader extends ClassLoader {
+    final String PATHMYCLASSES = "C:/myClasses/";
+
+    /**
+     * Поиск указанного класса и создание экземпляра этого класса
+     *
+     * @param name - указанный класс
+     * @return - экземпляр указанного класса
+     * @throws ClassNotFoundException
+     */
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         //Проверяем, существует ли такой файл
-        File className = new File(name+".class");
-        if(!className.isFile())
-            throw new ClassNotFoundException("Такого класса нет" + name);
-        try(InputStream ins = new BufferedInputStream(new FileInputStream(className))){
+        File className = new File(PATHMYCLASSES + name + ".class");
+        if (!className.isFile())
+            throw new ClassNotFoundException("Такого класса нет " + name);
+        try (InputStream ins = new BufferedInputStream(new FileInputStream(className))) {
             //Cчитываем файл в массив байтов
-            byte[]byteArray = new byte[(int)className.length()];
+            byte[] byteArray = new byte[(int) className.length()];
             ins.read(byteArray);
-            Class clazz = defineClass( byteArray, 0, byteArray.length);
+            log.debug("Класс успешно считан в массив байтов");
+            Class clazz = defineClass(name, byteArray, 0, byteArray.length);
+            log.debug("Массив байтов успешно преобразован в экземпляр класса");
             return clazz;
-        }catch (Exception e){
-            e.printStackTrace();
+        } catch (Exception e) {
+            log.error("Ошибки в байт коде", e);
             throw new ClassNotFoundException("Ошибки в байт коде");
         }
     }
