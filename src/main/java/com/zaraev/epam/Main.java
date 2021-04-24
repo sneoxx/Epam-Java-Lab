ec74dea4fa373d344dd8d4683bfdfcf7459b76aa
Rustam Zaraev <sneoxx@mail.ru>
2021-04-24 16:59:52 +0400

    Commit6 master fix

--- src/main/java/com/zaraev/epam/Main.java
+++ src/main/java/com/zaraev/epam/Main.java
@@ -1,10 +1,8 @@
 package com.zaraev.epam;
 
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 public class Main {
+
     public static void main(String[] args) {
-        log.debug("Старт программы");
+
     }
 }
--- src/test/java/com/zaraev/epam/MainTest.java
+++
@@ -1,4 +0,0 @@
-package com.zaraev.epam;
-
-public class MainTest {
-}
