19419174e76f4e27907981b90171d69bd6bef25d
Rustam Zaraev <sneoxx@mail.ru>
2021-05-09 10:51:23 +0400

    Commit3 lesson12 fix

--- Java-Zaraev-db/src/main/java/com/zaraev/epam/MainDb.java
+++ Java-Zaraev-db/src/main/java/com/zaraev/epam/MainDb.java
@@ -3,9 +3,8 @@ package com.zaraev.epam;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class Main {
+public class MainDb {
     public static void main(String[] args) {
-        System.out.println("dfdf");
 
     }
 }
