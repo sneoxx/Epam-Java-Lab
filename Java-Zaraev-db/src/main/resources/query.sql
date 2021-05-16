4b8796902d685ee4e99b76f33ded516a7f4e3bf2
Rustam Zaraev <sneoxx@mail.ru>
2021-05-16 09:04:33 +0400

    Commit3 lesson13-try1 +fix2

--- Java-Zaraev-db/src/main/resources/query.sql
+++ Java-Zaraev-db/src/main/resources/query.sql
@@ -2,15 +2,15 @@ SELECT * FROM products WHERE title = 'ACADEMY ACADEMY';
 
 SELECT * FROM products WHERE price = 9.99 and category = 8 ORDER BY category, price;
 
-SELECT * FROM products WHERE category = 8 or category = 15;
+SELECT * FROM products WHERE category in (8,15);
 
 SELECT * FROM products WHERE price BETWEEN 10 AND 20;
 
-SELECT * FROM orders WHERE orderdate BETWEEN '2004-01-05' AND '2004-02-05';
+SELECT * FROM orders WHERE orderdate BETWEEN to_date('2004-01-05', 'yyyy/MM/dd') AND to_date('2004-02-05', 'yyyy/MM/dd');
 
 SELECT customerid,COUNT(*) FROM orders GROUP BY customerid;
 
-SELECT SUM(totalamount),orderdate FROM orders GROUP BY customerid, orderdate HAVING SUM(totalamount) > 100 ORDER BY SUM(totalamount);
+SELECT customerid,orderdate,SUM(totalamount) FROM orders GROUP BY customerid, orderdate HAVING SUM(totalamount) > 100 ORDER BY SUM(totalamount);
 
 SELECT c.firstname, c.lastname, o.orderdate, p.title 
 FROM customers c 
